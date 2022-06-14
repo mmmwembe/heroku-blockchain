634ac4622c43ed5d9bcfc61645771ef133b3dcb7
Michael M <mmm111@hotmail.com>
2022-06-14 18:10:04 -0500

    changes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const http = require('http')
-const fs = require('fs')
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('templates/index.html').pipe(res)
-})
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({extend:true}));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
 
-server.listen(process.env.PORT || 3000)
+app.get('/', function(req, res){
+    res.render('index.html',{email:'mmm111@hotmail.com',password:'password'});
+});
