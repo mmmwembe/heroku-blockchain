1a6ec021d150d4864c3bcafd1beb2998a862e331
Michael M <mmm111@hotmail.com>
2022-06-14 17:49:35 -0500

    changes

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const router = express.Router();
-
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/", router);
 app.listen(process.env.port || 3000);
 
 console.log("Running at Port 3000");
